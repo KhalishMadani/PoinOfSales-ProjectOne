797af6758782d6b7878625e80e83d18748c8f66a
AuliaDewa <ichirou136@gmail.com>
2024-06-23 11:06:50 +0700

    task1

---
+++ projectPY.py
@@ -0,0 +1,22 @@
+variable_a = 'hello'
+
+# method itu pakai (fungsi)
+
+# index mulai dari 0
+list = ['2', '4', '6','8'] #mutable
+
+tuple = ('1', '3', '5','7') #immutable
+
+dictionary = {'Genap': list,
+              'Ganjil': tuple,
+              }
+
+def loop(parameter):
+    for x in parameter:
+        print(f'Bilangan: {x}')
+
+# tuple.append('murung')
+
+print(loop(dictionary['Ganjil'],&,['Genap']))
+
+#print(f'anda says : {variable_a}')
